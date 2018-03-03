ed3f6fe38183dcdc1de2fdf474e1d2e782616ec4
Sai <s.koturwar@gmail.com>
2018-03-03 13:40:07 +0900

    Second Commit

---
+++ DRAW.py
@@ -0,0 +1,8 @@
+""""
+Simple implementation of http://arxiv.org/pdf/1502.04623v2.pdf in TensorFlow
+Example Usage: 
+	python draw.py --data_dir=/tmp/draw --read_attn=True --write_attn=True
+Author: Saiprasad Koturwar
+"""
+
+from keras.layers import LSTM
