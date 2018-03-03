230be03eab484aebbe038eedfb408abfeea2a67f
Sai <s.koturwar@gmail.com>
2018-03-03 18:25:12 +0900

    Second Commit

--- DRAW.py
+++ DRAW.py
@@ -3,6 +3,27 @@ Simple implementation of http://arxiv.org/pdf/1502.04623v2.pdf in TensorFlow
 Example Usage: 
 	python draw.py --data_dir=/tmp/draw --read_attn=True --write_attn=True
 Author: Saiprasad Koturwar
+DISCLAIMER
+Work in progress.
 """
 
 from keras.layers import LSTM
+from keras import backend as K
+
+def 
+
+def read(attention=True,x,x_t,h_dec_prev):
+
+
+
+
+
+
+def encode(RNN_model,x,h_enc_prev):
+
+
+
+
+
+def decode(RNN_model,z,h_dec_prev):
+
